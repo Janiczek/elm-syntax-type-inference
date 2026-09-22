7aa85a43f853f6b22457ad9ae7b02d0516550d0c
Jeroen Engels <jfm.engels@gmail.com>
2026-09-22 11:23:42 +0200

    Convert to List only if needed

--- src/Elm/TypeInference/DependencySources.elm
+++ src/Elm/TypeInference/DependencySources.elm
@@ -129,7 +129,7 @@ neededSources deps sources =
                     supplied =
                         suppliedModuleNames package sources
 
-                    remaining : List String
+                    remaining : Set String
                     remaining =
                         docsModuleRefs pkg.modules
                             |> List.foldl
@@ -141,15 +141,15 @@ neededSources deps sources =
                                         Set.insert m acc
                                 )
                                 Set.empty
-                            |> Set.toList
-                            |> List.map ModuleNameExtra.dottedToFilePath
                 in
-                case remaining of
-                    [] ->
-                        needsSourcesAcc
+                if Set.isEmpty remaining then
+                    needsSourcesAcc
 
-                    _ :: _ ->
-                        ( package, remaining ) :: needsSourcesAcc
+                else
+                    ( package
+                    , List.map ModuleNameExtra.dottedToFilePath (Set.toList remaining)
+                    )
+                        :: needsSourcesAcc
             )
             []
 
