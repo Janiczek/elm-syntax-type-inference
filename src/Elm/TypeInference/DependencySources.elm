9ca862af8a969f5e8379902ce765b5539bafea21
Jeroen Engels <jfm.engels@gmail.com>
2026-09-22 11:10:16 +0200

    Avoid Set.union with an empty set

--- src/Elm/TypeInference/DependencySources.elm
+++ src/Elm/TypeInference/DependencySources.elm
@@ -105,11 +105,16 @@ neededSources deps sources =
                                 (\mod acc ->
                                     Dict.update mod.name
                                         (\existing ->
-                                            Just
-                                                (Set.union
-                                                    (Maybe.withDefault Set.empty existing)
-                                                    (documentedTypeNames mod)
-                                                )
+                                            case existing of
+                                                Just existing_ ->
+                                                    Just
+                                                        (Set.union
+                                                            (documentedTypeNames mod)
+                                                            existing_
+                                                        )
+
+                                                Nothing ->
+                                                    Just (documentedTypeNames mod)
                                         )
                                         acc
                                 )
